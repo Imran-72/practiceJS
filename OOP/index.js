e1378e281be8c1a0ee0cbb3220339451ebb12908
Imran Abdurakhmanov <petrow318@gmail.com>
2021-07-27 20:17:28 +0300

    c

--- OOP/index.js
+++ OOP/index.js
@@ -68,22 +68,34 @@
 
 // task 2
 
-class Person {
-    constructor(name, age) {
+// class Person {
+//     constructor(name, age) {
+//         this.name = name
+//         this.age = age
+//     }
+//
+//     compareAge(person) {
+//         if (person.age >= this.age) {
+//             alert(`${person.name} старше чем ${this.name}`)
+//         } else {
+//             alert(`${person.name} младше чем ${this.name}`)
+//         }
+//     }
+// }
+//
+// const person1 = new Person('Максим', 23);
+// const person2 = new Person('Светлана', 36);
+// const person3 = new Person('Ирина', 23);
+//
+// person1.compareAge(person2); // Максим младше, чем Светлана
+// person2.compareAge(person3); // Светлана старше, чем Ирина
+// person3.compareAge(person1); // Ирина младше, чем Максим
+
+// task 3
+
+class CarService {
+    constructor(name, workingHours) {
         this.name = name
-        this.age = age
-    }
-
-    compareAge() {
-
+        this.workingHours = workingHours
     }
 }
-
-const person1 = new Person('Максим', 24);
-const person2 = new Person('Светлана', 36);
-const person3 = new Person('Ирина', 23);
-
-for (let i = 0; i < )
-person1.compareAge(person2); // Максим младше, чем Светлана
-person2.compareAge(person3); // Светлана старше, чем Ирина
-person3.compareAge(person1); // Ирина младше, чем Максим
