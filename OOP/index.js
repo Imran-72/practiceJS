b114dd47df3b59bba945b6fe530b6474531814bc
Imran Abdurakhmanov <petrow318@gmail.com>
2021-07-28 20:36:18 +0300

    commit

--- OOP/index.js
+++ OOP/index.js
@@ -93,9 +93,88 @@
 
 // task 3
 
-class CarService {
-    constructor(name, workingHours) {
+// class CarService {
+//     static DefaultWorkingHours = {
+//         from: '9:00',
+//         till: '20:00',
+//     }
+//
+//     constructor(name, workingHours) {
+//         this.name = name
+//         this.workingHours = workingHours || CarService.DefaultWorkingHours
+//     }
+//
+//     repairCar(carName) {
+//         let fromTime = +this.workingHours.from.split(':00').join('')
+//         if (new Date().getHours() < fromTime) {
+//             alert(`К сожалению, мы сейчас закрыты. Приходите завтра`)
+//         } else if (new Date().getHours() > fromTime) {
+//             alert(`Сейчас отремонтируем вашу машину ${carName}! Ожидайте, пожалуйста`)
+//         } else if (!carName) {
+//             return console.error(`Вам необходимо указать название машины, чтобы ее отремонтировать`)
+//         }
+//     }
+//
+// }
+//
+// const carService = new CarService('RepairCarNow', {from: '12:00', till: '20:00'})
+// carService.repairCar('BMW')
+
+// task 4
+
+class Dictionary {
+    constructor(name) {
         this.name = name
-        this.workingHours = workingHours
+        this.words = {}
+    }
+
+    add(word, description) {
+        if (this.words.word !== word) {
+            this.words.word = word
+        }
+        if (this.words.description !== description) {
+            this.words.description = description
+        }
+    }
+
+    remove(word) {
+        if (this.words.word === word) {
+            delete this.words.word
+        }
+    }
+
+    get(word) {
+        if (this.words.word === word) {
+            return this.words.word
+        }
+    }
+
+    showAllWords() {
+        console.log(`${this.words.word} - ${this.words.description}`)
     }
 }
+
+const dictionary = new Dictionary('Толковый словарь')
+
+// task 5
+
+class HardWordsDictionary extends Dictionary {
+    constructor(name) {
+        super(name);
+    }
+
+    add(word, description) {
+        super.add(word, description)
+        this.words.word = word
+        this.words.description = description
+        this.words.isDifficult = true
+    }
+}
+
+const hardWordsDictionary = new HardWordsDictionary('Сложные слова')
+hardWordsDictionary.add('неологизм', 'Новое слово или выражение, а также новое значение старого слова.');
+hardWordsDictionary.showAllWords()
+hardWordsDictionary.remove('неологизм');
+
+
+
